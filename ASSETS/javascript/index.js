6b06f32c79abc19783ac8554e149efad97c7fb84
Victor <balogunaminat2017@example.com>
2025-09-18 18:36:08 +0100

    Update

--- ASSETS/javascript/index.js
+++ ASSETS/javascript/index.js
@@ -3,16 +3,21 @@ var themeBtn = document.querySelector("#theme-btn");
 var themeTag = document.querySelector("#theme");
 var lightMode = "ASSETS/styles/light.css";
 var darkMode = "ASSETS/styles/dark.css";
+var portfolioPic = document.querySelector("#portfolio-img");
 var themeStorage = localStorage.getItem("theme");
 themeBtn.addEventListener("click", () => {
   if (themeTag.getAttribute("href") === lightMode) {
     themeTag.setAttribute("href", darkMode);
     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
     localStorage.setItem("theme", darkMode);
+    portfolioPic.setAttribute("src" , "ASSETS/IMAGES/portfolio.png");
+
   } else {
     themeTag.setAttribute("href", lightMode);
     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
     localStorage.setItem("theme", lightMode);
+    portfolioPic.setAttribute("src" , "ASSETS/IMAGES/portfolio-light.png");
+
   }
 });
 // THEME STORING AND RETRIEVAL
@@ -21,13 +26,15 @@ switch (themeStorage) {
     themeTag.setAttribute("href", lightMode);
     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
     localStorage.setItem("theme", lightTheme);
+    portfolioPic.setAttribute("src" , "ASSETS/IMAGES/portfolio-light.png");
     break;
   case darkMode:
     themeTag.setAttribute("href", darkMode);
     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
     localStorage.setItem("theme", darkTheme);
+    portfolioPic.setAttribute("src" , "ASSETS/IMAGES/portfolio.png")
     break;
-  default:
+  default:;
 }
 
 
