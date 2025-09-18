bd9fca9bfdc0e6552f8aa705e8f15eb80da4587d
Victor <balogunaminat2017@example.com>
2025-09-18 12:23:29 +0100

    javascript update

--- ASSETS/javascript/index.js
+++ ASSETS/javascript/index.js
@@ -1,48 +1,33 @@
 // THEME HANDLING
 var themeBtn = document.querySelector("#theme-btn");
 var themeTag = document.querySelector("#theme");
-var lightMode =  "ASSETS/styles/light.css";
+var lightMode = "ASSETS/styles/light.css";
 var darkMode = "ASSETS/styles/dark.css";
 var themeStorage = localStorage.getItem("theme");
-themeBtn.addEventListener("click" , ()=> {
-    if(themeTag.getAttribute("href") === lightMode){
-        themeTag.setAttribute("href" , darkMode);
-        themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
-        localStorage.setItem("theme" , darkMode  );
-    }
-    else{
-       themeTag.setAttribute("href" , lightMode);
-        themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
-        localStorage.setItem("theme" , lightMode  ); 
-    }     
+themeBtn.addEventListener("click", () => {
+  if (themeTag.getAttribute("href") === lightMode) {
+    themeTag.setAttribute("href", darkMode);
+    themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
+    localStorage.setItem("theme", darkMode);
+  } else {
+    themeTag.setAttribute("href", lightMode);
+    themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
+    localStorage.setItem("theme", lightMode);
+  }
 });
 // THEME STORING AND RETRIEVAL
-switch(themeStorage){
-    case lightMode :   
-    themeTag.setAttribute("href" , lightMode);
+switch (themeStorage) {
+  case lightMode:
+    themeTag.setAttribute("href", lightMode);
     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
-    localStorage.setItem("theme" , lightTheme ); 
+    localStorage.setItem("theme", lightTheme);
     break;
-    case darkMode : 
-     themeTag.setAttribute("href" , darkMode);
+  case darkMode:
+    themeTag.setAttribute("href", darkMode);
     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
-    localStorage.setItem("theme" , darkTheme );
+    localStorage.setItem("theme", darkTheme);
     break;
-    default:;
+  default:
 }
 
 
-
-// if(localStorage.getItem("theme") === lightMode){
-//     themeTag.setAttribute("href" , lightMode);
-//     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
-//     localStorage.setItem("theme" , lightTheme ); 
-// }
-// else if(localStorage.getItem("theme") === darkMode){
-//     themeTag.setAttribute("href" , darkMode);
-//     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
-//     localStorage.setItem("theme" , darkTheme );
-// }
-// else{
-
-// }
