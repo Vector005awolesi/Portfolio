c95b7ccd4205b695e6d9dc7ca87ec3dd023b50f2
Victor <balogunaminat2017@example.com>
2025-09-18 10:38:16 +0100

    Javascript refactoring

--- ASSETS/javascript/index.js
+++ ASSETS/javascript/index.js
@@ -1,27 +1,29 @@
 // THEME HANDLING
-var lightThemeBtn = document.querySelector("#theme-btn");
+var themeBtn = document.querySelector("#theme-btn");
 var themeTag = document.querySelector("#theme");
-lightThemeBtn.addEventListener("click" , ()=> {
-    if(themeTag.getAttribute("href") === "ASSETS/styles/light.css"){
-        themeTag.setAttribute("href" , "ASSETS/styles/dark.css");
-        lightThemeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
-        localStorage.setItem("theme" , "ASSETS/styles/dark.css"  );
+var lightMode =  "ASSETS/styles/light.css";
+var darkMode = "ASSETS/styles/dark.css"
+themeBtn.addEventListener("click" , ()=> {
+    if(themeTag.getAttribute("href") === lightMode){
+        themeTag.setAttribute("href" , darkMode);
+        themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
+        localStorage.setItem("theme" , darkMode  );
     }
     else{
-       themeTag.setAttribute("href" , "ASSETS/styles/light.css");
-        lightThemeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
-        localStorage.setItem("theme" , "ASSETS/styles/light.css"  ); 
+       themeTag.setAttribute("href" , lightMode);
+        themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
+        localStorage.setItem("theme" , lightMode  ); 
     }     
 });
 // THEME STORING AND RETRIEVAL
-if(localStorage.getItem("theme") === "ASSETS/styles/light.css"){
-    themeTag.setAttribute("href" , "ASSETS/styles/light.css");
-    lightThemeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
+if(localStorage.getItem("theme") === lightMode){
+    themeTag.setAttribute("href" , lightMode);
+    themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
     localStorage.setItem("theme" , lightTheme ); 
 }
-else if(localStorage.getItem("theme") === "ASSETS/styles/dark.css"){
-    themeTag.setAttribute("href" , "ASSETS/styles/dark.css");
-    lightThemeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
+else if(localStorage.getItem("theme") === darkMode){
+    themeTag.setAttribute("href" , darkMode);
+    themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
     localStorage.setItem("theme" , darkTheme );
 }
 else{
