03c316d7d70c70d1002da14c48990d28062d70d8
Victor <balogunaminat2017@example.com>
2025-09-18 12:22:38 +0100

    javascript update

--- ASSETS/javascript/index.js
+++ ASSETS/javascript/index.js
@@ -2,7 +2,8 @@
 var themeBtn = document.querySelector("#theme-btn");
 var themeTag = document.querySelector("#theme");
 var lightMode =  "ASSETS/styles/light.css";
-var darkMode = "ASSETS/styles/dark.css"
+var darkMode = "ASSETS/styles/dark.css";
+var themeStorage = localStorage.getItem("theme");
 themeBtn.addEventListener("click" , ()=> {
     if(themeTag.getAttribute("href") === lightMode){
         themeTag.setAttribute("href" , darkMode);
@@ -16,16 +17,32 @@ themeBtn.addEventListener("click" , ()=> {
     }     
 });
 // THEME STORING AND RETRIEVAL
-if(localStorage.getItem("theme") === lightMode){
+switch(themeStorage){
+    case lightMode :   
     themeTag.setAttribute("href" , lightMode);
     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
     localStorage.setItem("theme" , lightTheme ); 
-}
-else if(localStorage.getItem("theme") === darkMode){
-    themeTag.setAttribute("href" , darkMode);
+    break;
+    case darkMode : 
+     themeTag.setAttribute("href" , darkMode);
     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
     localStorage.setItem("theme" , darkTheme );
+    break;
+    default:;
 }
-else{
 
-}
+
+
+// if(localStorage.getItem("theme") === lightMode){
+//     themeTag.setAttribute("href" , lightMode);
+//     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-moon'><i>";
+//     localStorage.setItem("theme" , lightTheme ); 
+// }
+// else if(localStorage.getItem("theme") === darkMode){
+//     themeTag.setAttribute("href" , darkMode);
+//     themeBtn.innerHTML = "<i class = 'fas fa-sharp fa-sun'><i>";
+//     localStorage.setItem("theme" , darkTheme );
+// }
+// else{
+
+// }
